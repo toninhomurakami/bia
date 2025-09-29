18c19d10ce7a39ee9896657d2a4b652f3c38e3b2
toninhomurakami <toninhomurakami@gmail.com>
2025-09-29 10:32:08 -0300

    Update lancar_ec2_zona_a.sh
    Restore ao original

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -7,8 +7,8 @@ if [ -z "$security_group_id" ]; then
     exit 1
 fi
 
-aws ec2 run-instances --image-id ami-08982f1c5bf93d976 --count 1 --instance-type t3.micro \
+aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro \
 --security-group-ids $security_group_id --subnet-id $subnet_id --associate-public-ip-address \
---block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":40,"VolumeType":"gp3"}}]' \
+--block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":15,"VolumeType":"gp2"}}]' \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bia-dev}]' \
 --iam-instance-profile Name=role-acesso-ssm --user-data file://user_data_ec2_zona_a.sh
